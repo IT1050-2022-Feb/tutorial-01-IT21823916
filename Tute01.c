9afa3cde294b94171ebfc325ef6131a39704da6c
IT21823916 <110651540+IT21823916@users.noreply.github.com>
2022-08-06 08:10:14 +0000

    Tutorial 1 done

--- Tute01.c
+++ Tute01.c
@@ -2,9 +2,24 @@
 
    Write a C program to input marks of two subjects. Calculate and print the average of the two marks. */
 
-#include <stdio.h>
+##include <iostream>
+using namespace std;
+
 
 int main() {
+  int firstsub,secondsub,total,average;
+	
+	cout<<"Enter first subject mark: ";
+	cin>>firstsub;
+	
+	cout<<"Enter second subject mark: ";
+	cin>>secondsub;
+	
+	total=firstsub+secondsub;
+	average=total/2;
+	
+	cout<<"Total is: "<<total<<endl;
+	cout<<"Average is: "<<average<<endl;
   
   return 0;
 }
--- Tute02.c
+++ Tute02.c
@@ -16,9 +16,25 @@ Amount = 20 x 50 = 1000
 Distance -> 50
 Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
-#include <stdio.h>
+#include <iostream>
+using namespace std;
+
 
 int main() {
-  
+  int distance,total;
+	
+	cout<<"Enter travel Distance: ";
+	cin>>distance;
+	
+	if(distance<30){
+		total=distance*50;
+		cout<<"Total cost is: "<<total<<endl;
+	}
+	else
+	if(distance>30){
+		total=30*50+(distance-30)*40;
+		cout<<"Total cost is: "<<total<<endl;
+	}
+	
   return 0;
 }
